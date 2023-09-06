f1ab728e843383b4d7037f256d5d46ab08961c21
Code_Developer23 <dev23@finoservices.com>
2023-09-06 13:51:06 -0500

    V1.0.1 Se crea el scroll para add mas pokemon Se crea la constante de colores Se crea la funcion para etiquetar pokemon por coilor

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from "react-native";
+import { capitalize } from "lodash";
+import getColorByPokemonType from "../utils/getColorByPokemonType";
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
+  
+  const pokemonColor = getColorByPokemonType(pokemon.type)
+  const bgStyles = {backgroundColor: pokemonColor, ...styles.bgStyles}
 
   const goToPokemon = () => {
     console.log(`Vamos al Pokemon: ${pokemon.name}`)
@@ -12,10 +17,12 @@ export default function PokemonCard(props) {
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyle}>
-            <Text style={styles.name}>{pokemon.name}</Text>
-            <Image 
-              source={{uri: pokemon.image}}
+          <View style={bgStyles}>
+            <Text style={styles.number}>#{`${pokemon.order}`.padStart(3, 0)}</Text>
+            <Text style={styles.name}>{capitalize(pokemon.name)}</Text>
+            <Text style={styles.type}>{capitalize(pokemon.type)}</Text>
+            <Image
+              source={{ uri: pokemon.image }}
               style={styles.image}
             />
           </View>
@@ -33,10 +40,19 @@ const styles = StyleSheet.create({
   },
   spacing: {
     flex: 1,
-    padding: 55
+    padding: 5
   },
-  bgStyle: {
-    backgroundColor: "gray"
+  bgStyles:{
+    flex: 1,
+    borderRadius: 15,
+    padding: 10
+  },
+  number: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+    color: "white",
+    fontSize: 11
   },
   name: {
     color: "white",
@@ -44,7 +60,12 @@ const styles = StyleSheet.create({
     fontSize: 15,
     paddingTop: 10,
   },
-  image:{
+  type:{
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  image: {
     position: "absolute",
     bottom: 2,
     right: 2,
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -3,24 +3,26 @@ import { SafeAreaView } from "react-native";
 import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokeApi";
 import PokemonList from "../components/PokemonList";
 
-
-
 export default function Pokedex() {
   const [pokemons, setPokemons] = useState([])
-  // Load pokemons from API
+  const [nextUrl, setNextUrl] = useState(null)
+  
   useEffect(() => {
     (async () => {
       await loadPokemons();
     })()
   }, [])
 
-  const loadPokemons = async () => {
+  const loadPokemons = async  () => {
     try {
-      const res = await getPokemonsApi();
+    
+      const res = await getPokemonsApi(nextUrl);
+      setNextUrl(res.next)
+
       const pokemonArray = []
-      for await (const pokemon of res.results){
+      for await (const pokemon of res.results) {
         const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url)
-       
+
         pokemonArray.push({
           id: pokemonDetails.id,
           name: pokemonDetails.name,
@@ -38,7 +40,7 @@ export default function Pokedex() {
 
   return (
     <SafeAreaView>
-      <PokemonList pokemons={pokemons} />
+      <PokemonList pokemons={pokemons} loadPokemons={loadPokemons} isNext={nextUrl} />
     </SafeAreaView>
   )
 }
