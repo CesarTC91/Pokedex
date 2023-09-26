8909ff42829939f017bd4a60d0bfc12b51e35467
Code_Developer23 <dev23@finoservices.com>
2023-09-26 09:26:59 -0500

    Se crean datos fictios para el Login Se crea el componente para LoginForm Se crea el componente para UserData Se vinculan los componentes a la Screen Account

---
+++ src/utils/userDB.js
@@ -0,0 +1,13 @@
+import { first } from "lodash"
+
+export const user = {
+  username: 'polaco91',
+  password: 'polaco1291//'
+}
+
+export const userDetails = {
+  username: 'polaco91',
+  firstName: 'Cesar',
+  lastName: 'Tobon',
+  email: 'cestobon991@gmail.com'
+}
